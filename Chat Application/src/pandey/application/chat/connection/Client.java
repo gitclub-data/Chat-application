7154c1834cd5692a68342ae0b836a58e857172e9
Gaurav pandey <67181343+gitclubdata@users.noreply.github.com>
2021-11-16 10:10:57 +0530

    server to client connection

--- Chat Application/src/pandey/application/chat/connection/Client.java
+++ Chat Application/src/pandey/application/chat/connection/Client.java
@@ -1,5 +1,26 @@
 package pandey.application.chat.connection;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Client {
+	//defining port number and ip address
+	private static final int port=9090;
+	private static final String ip_address="127.0.0.1";
+	public static void main(String args[]) throws UnknownHostException, IOException {
+		
+		// set connection to server
+		Socket socket= new Socket(ip_address,port);
+		
+		// make a input streamreader to get input from skocket
+		BufferedReader inp=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		System.out.println(inp.readLine());		
+		
+		//closing the connection
+		inp.close();
+		socket.close();		
+	}
 }
--- Chat Application/src/pandey/application/chat/connection/Server.java
+++ Chat Application/src/pandey/application/chat/connection/Server.java
@@ -1,5 +1,27 @@
 package pandey.application.chat.connection;
 
-public class Server {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+	//defining port number
+	private static final int port=9090;
+	public static void main(String args[]) throws IOException {
+		//start the server
+		ServerSocket listener=new ServerSocket(port);
+
+		System.out.println("Waiting for connection");
+		Socket client=listener.accept(); 
+		System.out.println("Connected");
+		
+		//make a output stream
+		PrintWriter out=new PrintWriter(client.getOutputStream(),true);
+		out.println("hello");
+		
+		System.out.print("Closing Connection");
+		client.close();
+		listener.close();
+	}
 }
